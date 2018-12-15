5526b4b33ee80f806f4ecbcf24fbd252aca085ec
1000ch <shogo.sensui@gmail.com>
2018-12-16 01:11:25 +0900

    Remove unused file

--- app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#install').addEventListener('click', () => {
-    chrome.webstore.install('', () => {
-      console.log('installed');
-    }, error => {
-      console.error(error);
-    });
-  });
-});
